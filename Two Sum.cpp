2565141afcf797bb8cdf16a6d115df878094e593
chenzhg33 <chenzhg33@mail2.sysu.edu.cn>
2014-12-22 18:39:01 +0800

    change shiftwidth

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -5,22 +5,21 @@
 using namespace std;
 class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-			map<int, int> m;
-			vector<int> result;
-			int size = numbers.size();
-			for (int i = 0; i < size; ++i) {
-				if (m.find(numbers[i]) == m.end()) {
-					m[target - numbers[i]] = i;
-				} else {
-					result.push_back(m[numbers[i]] + 1);
-					result.push_back(i + 1);
-					break;
-				}
+	vector<int> twoSum(vector<int> &numbers, int target) {
+		map<int, int> m;
+		vector<int> result;
+		int size = numbers.size();
+		for (int i = 0; i < size; ++i) {
+			if (m.find(numbers[i]) == m.end()) {
+				m[target - numbers[i]] = i;
+			} else {
+				result.push_back(m[numbers[i]] + 1);
+				result.push_back(i + 1);
+				break;
 			}
-			return result;
-    }
-
+		}
+		return result;
+  }
 };
 int main() {
 	Solution solution;
