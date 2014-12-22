f85826d43312697d5b286ab4c14f8def1725f7ec
chenzhg33 <chenzhg33@mail2.sysu.edu.cn>
2014-12-22 18:28:12 +0800

    2014-12-17

---
+++ Reverse Integer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+			const int M = 2147483647;
+			const int N = -2147483648;
+			int result = 0;
+			int b;
+			while (x != 0) {
+				b = x % 10;
+				if (b > M/10 || b < N/10) {
+					return 0;
+				}
+				x /= 10;
+				result *= 10;
+				result += b;
+			}
+			return result;
+    }
+};
+int main() {
+	Solution solution;
+	while (1) {
+		int x;
+		cin >> x;
+		cout << solution.reverse(x) << endl;
+	}
+}
---
+++ Two Sum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+			map<int, int> m;
+			vector<int> result;
+			int size = numbers.size();
+			for (int i = 0; i < size; ++i) {
+				if (m.find(numbers[i]) == m.end()) {
+					m[target - numbers[i]] = i;
+				} else {
+					result.push_back(m[numbers[i]] + 1);
+					result.push_back(i + 1);
+					break;
+				}
+			}
+			return result;
+    }
+
+};
+int main() {
+	Solution solution;
+	int a[] = {2, 7, 11, 15};
+	vector<int> vec(a, a + 4);
+	vector<int> re = solution.twoSum(vec, 26);
+	if (re.size() != 0) {
+		cout << re[0] << ' ' << re[1] << endl;
+	}
+	system("pause");
+	return 0;
+}
